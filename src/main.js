bb8ca4492564719b01fc457f40fde47260dfc811
Marouane <57333995+riimuru@users.noreply.github.com>
2024-02-16 18:23:07 -0500

    feat: add zillow rentals route

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import express from 'express';
 
+import { getRentals } from './zillow.js';
+
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/zillow/:borough', (req, res) => {
+  const borough = req.params.borough;
+  const rentals = getRentals(borough);
+  res.json(rentals);
 });
 
 app.listen(3000, () => {
