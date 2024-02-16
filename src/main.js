317db2b56ca55bcf4a11fc8f3af4b88c8dacdfdc
Marouane <57333995+riimuru@users.noreply.github.com>
2024-02-16 18:26:01 -0500

    fix files

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { getRentals } from './zillow.js';
+import Zillow from './providers/zillow.js';
 
 const app = express();
 app.use(express.json());
+
+const zillow = new Zillow();
 
 app.get('/', (req, res) => {
   res.send('Hello World');
@@ -11,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.get('/zillow/:borough', (req, res) => {
   const borough = req.params.borough;
-  const rentals = getRentals(borough);
+  const rentals = zillow.getRentals(borough);
   res.json(rentals);
 });
 
